0349dd5bb8b85291e0e3879ccf3194872dd6c242
kabrants <kaueefonsecs@gmail.com>
2026-02-13 16:02:53 -0300

    While

---
+++ Aula12/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Ex1: 
+
+        //int i = 0;
+
+        //while (i <= 5)
+        //{
+        //    Console.WriteLine(i);
+                //int++;
+
+        //}
+
+
+        //Ex2: 
+        double budget = 100;
+        while (budget > 0)
+        {
+            Console.WriteLine(budget);
+            //Realizando alguma operação
+
+            budget--;
+        }
+
+        Console.WriteLine("Atenção! Saldo Insuficiente.");
+    }
+
+}
